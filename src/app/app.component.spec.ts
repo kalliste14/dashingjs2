578a05255dfa156bc178888ccf67913834f6db44
kalliste14 <mailtest@bbox.fr>
2020-01-04 12:17:42 +0100

    docs(ep19): mise en place des routes - add DashBoardModule component - add PageNotFound component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('dashingjs2');
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to dashingjs2!');
-  });
 });
