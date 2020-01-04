f9ddf53528307ca57985341f4725d70789388f6f
kalliste14 <mailtest@bbox.fr>
2020-01-04 15:42:46 +0100

    docs(ep20): mise en place de gridster - installation du module - configuration - définition du dashboard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { GridsterModule} from 'angular-gridster2';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [AppComponent],
-      imports: [RouterTestingModule]
+      imports: [RouterTestingModule, GridsterModule]
     }).compileComponents();
   }));
 
