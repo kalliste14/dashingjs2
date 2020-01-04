ce7223d5f7595ac9895d9ee39384ad4697b5b7d8
kalliste14 <mailtest@bbox.fr>
2020-01-04 15:47:57 +0100

    docs(ep20): mise en place de gridster - installation du module - configuration - définition du dashboard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
-import { GridsterModule} from 'angular-gridster2';
+import { GridsterModule } from 'angular-gridster2';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -10,16 +10,4 @@ describe('AppComponent', () => {
       imports: [RouterTestingModule, GridsterModule]
     }).compileComponents();
   }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'dashingjs2'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('dashingjs2');
-  });
 });
